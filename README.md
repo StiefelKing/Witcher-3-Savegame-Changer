95cbc929529be052a04e7788dd09d667339b276d
StiefelKing <der@getzler.de>
2020-07-16 16:57:32 +0200

    made everything more pythonic and object orientated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # The Witcher 3 Savegame Changer
-This little Program helps you to use up to nine Savegame "Profiles" in The Witcher 3.
+This little Program helps you to use as many Savegame "Profiles" in The Witcher 3 as you like.
 Place the exe file in your "The Witcher 3" folder and run it. 
 Then follow the setup instructions.
--- SavegameChanger.py
+++ SavegameChanger.py
@@ -5,25 +5,34 @@ import datetime
 
 
 class Profile:
-    counter = 0
 
-    def __init__(self, name, number, path):
-        if name or number or path is None:
-            print("")
-            # todo constructor for object from prompt
-            Profile.counter += 1
+    def __init__(self, name_const, number_const, path_const):
+        self.name = name_const
+        self.number = number_const
+        if path_const is None:
+            # for objects from prompt
+            self.path = "profiles/" + name_const
         else:
-            # constructor for objects from .ini
-            self.name = name
-            self.number = number
-            self.path = path
+            # for objects from .ini
+            self.path = path_const
+
+    def export_config(self):
+        config = configparser.ConfigParser()
+        config[str(self.number)] = {"Name": self.name, "Path": self.path}
+        with open("SavegameChanger.ini", "a") as configfile:
+            config.write(configfile)
+
+    def compare_number(self, number):
+        if self.number == number:
+            return True
+        else:
+            return False
 
 
 # moves the correct profile into the game folder
-def move_files(variables, variables_backup):
-    backup_files(variables_backup)
-    print("Loading {}, named".format(variables[0], variables[1]))
-    source = variables[2]
+def change_savegame(profile_local):
+    print("Loading Profile {}, named {}".format(profile_local.number, profile_local.name))
+    source = profile_local.path
     destination = "gamesaves"
     files = os.listdir(source)
     for f in files:
@@ -31,10 +40,10 @@ def move_files(variables, variables_backup):
 
 
 # makes a backup of both the previous files in the profile folder
-def backup_files(variables):
-    print("Backing up {}, named {}.".format(variables[0], variables[1]))
-    source1 = variables[2]
-    destination1 = "profiles/_Backup/{}_{}/".format(variables[1], datetime.datetime.now().timestamp())
+def backup_savegames(profile_local):
+    print("Backing up Profile {}, named {}.".format(profile_local.number, profile_local.name))
+    source1 = profile_local.path
+    destination1 = "profiles/_Backup/{}_{}/".format(profile_local.name, datetime.datetime.now().timestamp())
     if not os.path.exists(destination1):
         os.makedirs(destination1)
     files = os.listdir(source1)
@@ -42,7 +51,7 @@ def backup_files(variables):
         move(source1 + "/" + f, destination1)
     print("Backing up gamesaves folder")
     source2 = "gamesaves"
-    destination2 = "profiles/{}/".format(variables[1])
+    destination2 = "profiles/{}/".format(profile_local.name)
     if not os.path.exists(destination2):
         os.makedirs(destination2)
     files = os.listdir(source2)
@@ -50,14 +59,48 @@ def backup_files(variables):
         move(source2 + "/" + f, destination2)
 
 
+def get_profile(profiles_local, number):
+    for profile_local in profiles_local:
+        if profile_local.compare_number(number):
+            return profile_local
+
+
+def choose(profiles_local, max_choice_local):
+    success_choice = False
+    while not success_choice:
+        try:
+            choice = int(input("Choice: (number): "))
+            if int(choice) == 0:
+                exit(0)
+            elif max_choice_local >= choice > 0:
+                success_choice = True
+                backup_savegames(get_profile(profiles_local, check_active()))
+                change_savegame(get_profile(profiles_local, choice))
+        except ValueError:
+            print("invalid Choice")
+
+
 # checks which profile is loaded right now
 def check_active():
     files = os.listdir("gamesaves")
-    active_profile = "0"
     for file in files:
         if file.endswith(".sc"):
-            active_profile = file
-    return int(active_profile[0])
+            return int(file[0:len(file) - 3])
+    print("no active Profile found")
+    exit(-1)
+
+
+# prints out the information on the profiles, numbers and choices
+def prompt(profiles_local):
+    print("What would you like to do? ")
+    print("To rerun the initial setup delete or move the .ini file.")
+    print("the currently active Profile is named {}".format(check_active()))
+    print("0 exit")
+    i = 0
+    for profile in profiles_local:
+        i += 1
+        print(str(i) + " load profile {}, named {}".format(profile.number, profile.name))
+    return i
 
 
 # initializes the program.
@@ -70,53 +113,34 @@ def initialize_program():
             success_init = True
         except FileNotFoundError:
             setup_ini()
-    variables = []
+    profiles_local = []
     for section in config.sections():
-        profile_local = [section, config[section]["name"], config[section]["path"]]
-        variables.append(profile_local)
-    return variables
+        profiles_local.append(Profile(section, config[section]["name"], config[section]["path"]))
+    return profiles_local
 
 
 # creates the necessary ini file and allows you to configure your Profiles
 def setup_ini():
     amount = input("How many profiles do you want? ")
-    directories = {"profiles", "profiles/_backup"}
+    directories = ["profiles", "profiles/_backup"]
+    files = []
+    for number in range(1, int(amount) + 1):
+        name_local = input("What name should profile " + str(number) + " have? ")
+        profile_local = Profile(name_local, number, None)
+        profile_local.export_config()
+        directories.append(profile_local.path)
+        files.append([profile_local.number, profile_local.path])
     for directory in directories:
         if not os.path.exists(directory):
             os.makedirs(directory)
-    config = configparser.ConfigParser()
-    for number in range(1, int(amount) + 1):
-        name_local = input("What name should profile " + str(number) + " have? ")
-        config["Profile " + str(number)] = {"Name": name_local,
-                                            "Path": "profiles/" + name_local}
-        if not os.path.exists("profiles/" + name_local):
-            os.makedirs("profiles/" + name_local)
-            open("profiles/" + name_local + "/{}.sc".format(str(number)), "w+")
-    with open("SavegameChanger.ini", "w") as configfile:
-        config.write(configfile)
+    if not os.path.exists("gamesaves"):
+        os.makedirs("gamesaves")
     current = input("Which profile is the currently active one? (number) ")
-    open("gamesaves/{}.sc".format(current), "w+")
+    files.append([current, "gamesaves"])
+    for file in files:
+        open(file[1] + "/{}.sc".format(file[0]), "w+")
 
 
-initialized = initialize_program()
-print("What would you like to do? ")
-print("To rerun the initial setup delete or move the .ini file.")
-print("the currently active Profile is named {}".format(check_active()))
-print("0 exit")
-i = 0
-for profile, name, path in initialized:
-    i += 1
-    print(str(i) + " load {}, named {}".format(profile, name))
-max_choice = i
-success_choice = False
-while not success_choice:
-    try:
-        choice = input("Choice: (number): ")
-        choice = int(choice)
-        if int(choice) == 0:
-            exit(0)
-        elif max_choice >= choice > 0:
-            success_choice = True
-            move_files(initialized[choice - 1], initialized[check_active() - 1])
-    except ValueError:
-        print("invalid Choice")
+profiles = initialize_program()
+max_choice = prompt(profiles)
+choose(profiles, max_choice)
